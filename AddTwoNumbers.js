f635ca18c6c2f79b075bc55b3cde48fc6fbb7c9b
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-02-09 09:53:02 -0800

    create one ListNode

--- AddTwoNumbers.js
+++ AddTwoNumbers.js
@@ -16,3 +16,11 @@ function ListNode(val, next) {
 const addTwoNumbers = function(l1, l2) {
     
 };
+
+console.log("******************************");
+console.log("Add Two Numbers\n");
+
+let listNode1A = new ListNode(3);
+let listNode1B = new ListNode(4, listNode1A);
+let listNode1C = new ListNode(2, listNode1B);
+console.log(listNode1C);
