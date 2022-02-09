ead0775d85dea619609ed893050214c3cf868c14
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-02-09 10:09:23 -0800

    add params L1 and L2

--- AddTwoNumbers.js
+++ AddTwoNumbers.js
@@ -14,7 +14,9 @@ function ListNode(val, next) {
  * @return {ListNode}
  */
 const addTwoNumbers = function(l1, l2) {
-    
+    console.log("In add two numbers");
+    console.log("L1", l1);
+    console.log("L2", l2);
 };
 
 console.log("******************************");
@@ -23,4 +25,11 @@ console.log("Add Two Numbers\n");
 let listNode1A = new ListNode(3);
 let listNode1B = new ListNode(4, listNode1A);
 let listNode1C = new ListNode(2, listNode1B);
-console.log(listNode1C);
+// console.log(listNode1C);
+
+let listNode2A = new ListNode(4);
+let listNode2B = new ListNode(6, listNode2A);
+let listNode2C = new ListNode(5, listNode2B);
+// console.log(listNode2C);
+
+addTwoNumbers(listNode1C, listNode2C);
