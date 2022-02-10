e9ace537aa6a1a2e27c86a5fa427bdef3e61c378
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-02-10 02:35:17 -0800

    iterate through the lists

--- AddTwoNumbers.js
+++ AddTwoNumbers.js
@@ -14,23 +14,33 @@ function ListNode(val, next) {
  * @return {ListNode}
  */
 const addTwoNumbers = function(l1, l2) {
-    let list1 = l1, lest2=l2;
-    console.log (list1, list2)
+    let output = null;
+    let list1 = l1, list2=l2;
+    let carry = 0;
+    // console.log (list1, list2)
+    while ( (list1 !== null) && (list2 !== null)) {
+        let sum = carry + list1.val + list2.val;
+        output = new ListNode( sum%10, output );
+        carry = Math.floor( sum/10 );
+        list1 = list1.next;
+        list2 = list2.next;
+        console.log("output", output);
+    }
 };
 
 console.log("******************************");
 console.log("Add Two Numbers\n");
 
-let listNode1A = new ListNode(7);
-let listNode1B = new ListNode(0, listNode1A);
-let listNode1C = new ListNode(0, listNode1B);
-listNode1C = new ListNode(0, listNode1C);
-listNode1C = new ListNode(0, listNode1C);
-listNode1C = new ListNode(0, listNode1C);
-listNode1C = new ListNode(0, listNode1C);
-listNode1C = new ListNode(0, listNode1C);
-listNode1C = new ListNode(0, listNode1C);
-listNode1C = new ListNode(1, listNode1C);
+let listNode1A = new ListNode(1);
+let listNode1B = new ListNode(3, listNode1A);
+let listNode1C = new ListNode(4, listNode1B);
+// listNode1C = new ListNode(0, listNode1C);
+// listNode1C = new ListNode(0, listNode1C);
+// listNode1C = new ListNode(0, listNode1C);
+// listNode1C = new ListNode(0, listNode1C);
+// listNode1C = new ListNode(0, listNode1C);
+// listNode1C = new ListNode(0, listNode1C);
+listNode1C = new ListNode(2, listNode1C);
 // console.log(listNode1C);
 
 let listNode2A = new ListNode(4);
