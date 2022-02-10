da2cbb46438a480bb80c175625e0b8bf4fa46ed7
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-02-10 01:49:27 -0800

    update

--- AddTwoNumbers.js
+++ AddTwoNumbers.js
@@ -14,51 +14,57 @@ function ListNode(val, next) {
  * @return {ListNode}
  */
 const addTwoNumbers = function(l1, l2) {
-    // console.log("In add two numbers");
-    // console.log("L1", l1);
-    // console.log("L2", l2);
-    let numL1 = 0;
+    let numL1 = ""
     let runner = l1;
-    let multiplier = 1;
-    let arrayL1 = [];
+    // let multiplier = 1;
 
     // Retrieve l1 number
     while (runner !== null) {
-        numL1 = numL1 + ( runner.val * multiplier );
-        multiplier = multiplier * 10;
+        numL1 = runner.val + numL1;
+        // multiplier = multiplier * 10;
         runner = runner.next;
     }
 
-    let numL2 = 0;
+    let numL2 = "";
     runner = l2;
-    multiplier = 1;
+    // multiplier = 1;
 
     // Retrieve L2 number
     while (runner !== null) {
-        numL2 = numL2 + ( runner.val * multiplier );
-        multiplier = multiplier * 10;
+        numL2 = runner.val + numL2;
+        // multiplier = multiplier * 10;
         runner = runner.next;
     }
 
-    // console.log("L1", numL1, "numL2", numL2);
-    let sum = (numL1 + numL2).toString();
-    console.log("Sum:", typeof(sum) );
-    
-
-
+    // Get the sum of the two numbers then convert into a SLL
+    let sum = (parseInt(numL1) + parseInt(numL2)).toString();
+    console.log("sum", sum);
+    let output = null;
+    for ( let idx=0; idx<sum.length; idx++ ) {
+        output = new ListNode( parseInt(sum[idx]), output );
+        console.log(idx,output);
+    }
+    return output;
 };
 
 console.log("******************************");
 console.log("Add Two Numbers\n");
 
-let listNode1A = new ListNode(3);
-let listNode1B = new ListNode(4, listNode1A);
-let listNode1C = new ListNode(2, listNode1B);
-// console.log(listNode1C);
+let listNode1A = new ListNode(7);
+let listNode1B = new ListNode(0, listNode1A);
+let listNode1C = new ListNode(0, listNode1B);
+listNode1C = new ListNode(0, listNode1C);
+listNode1C = new ListNode(0, listNode1C);
+listNode1C = new ListNode(0, listNode1C);
+listNode1C = new ListNode(0, listNode1C);
+listNode1C = new ListNode(0, listNode1C);
+listNode1C = new ListNode(0, listNode1C);
+listNode1C = new ListNode(1, listNode1C);
+console.log(listNode1C);
 
 let listNode2A = new ListNode(4);
 let listNode2B = new ListNode(6, listNode2A);
 let listNode2C = new ListNode(5, listNode2B);
-// console.log(listNode2C);
+console.log(listNode2C);
 
-addTwoNumbers(listNode1C, listNode2C);
+console.log(addTwoNumbers(listNode1C, listNode2C));
