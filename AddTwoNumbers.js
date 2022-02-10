7f46b901e2d3224447241186e4122bd41932cd18
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-02-10 02:15:46 -0800

    start over

--- AddTwoNumbers.js
+++ AddTwoNumbers.js
@@ -14,34 +14,8 @@ function ListNode(val, next) {
  * @return {ListNode}
  */
 const addTwoNumbers = function(l1, l2) {
-    let numL1 = ""
-    let runner = l1;
-
-    // Retrieve l1 number
-    while (runner !== null) {
-        numL1 = runner.val + numL1;
-        runner = runner.next;
-    }
-
-    let numL2 = "";
-    runner = l2;
-
-    // Retrieve L2 number
-    while (runner !== null) {
-        numL2 = runner.val + numL2;
-        // multiplier = multiplier * 10;
-        runner = runner.next;
-    }
-
-    // Get the sum of the two numbers then convert into a SLL
-    let sum = BigInt(parseInt(numL1) + parseInt(numL2))).toString();
-    console.log("sum", sum);
-    let output = null;
-    for ( let idx=0; idx<sum.length-1; idx++ ) {
-        output = new ListNode( parseInt(sum[idx]), output );
-        // console.log(idx,output);
-    }
-    return output;
+    let list1 = l1, lest2=l2;
+    console.log (list1, list2)
 };
 
 console.log("******************************");
@@ -57,11 +31,11 @@ listNode1C = new ListNode(0, listNode1C);
 listNode1C = new ListNode(0, listNode1C);
 listNode1C = new ListNode(0, listNode1C);
 listNode1C = new ListNode(1, listNode1C);
-console.log(listNode1C);
+// console.log(listNode1C);
 
 let listNode2A = new ListNode(4);
 let listNode2B = new ListNode(6, listNode2A);
 let listNode2C = new ListNode(5, listNode2B);
-console.log(listNode2C);
+// console.log(listNode2C);
 
 console.log(addTwoNumbers(listNode1C, listNode2C));
